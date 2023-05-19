534314469ca3c2267372e6569fa01eb99f828251
eskrm <eskrm@pm.me>
2023-05-19 14:04:12 -0700

    Do not grant unrestricted access to new databases

--- src/metabase/models/database.clj
+++ src/metabase/models/database.clj
@@ -54,9 +54,9 @@
 (defn- post-insert [database]
   (u/prog1 database
     ;; add this database to the All Users permissions group
-    (perms/grant-full-data-permissions! (perms-group/all-users) database)
+    ;;(perms/grant-full-data-permissions! (perms-group/all-users) database)
     ;; give full download perms for this database to the All Users permissions group
-    (perms/grant-full-download-permissions! (perms-group/all-users) database)
+    ;;(perms/grant-full-download-permissions! (perms-group/all-users) database)
     ;; schedule the Database sync & analyze tasks
     (schedule-tasks! database)))
 
